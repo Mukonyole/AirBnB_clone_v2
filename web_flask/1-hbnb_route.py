af78f44432772c4f6a343dc9d9cb19f01b99d8ac
Evans Mukonyole <emukonyole@gmail.com>
2024-07-26 02:02:35 -0700

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,19 +4,21 @@ A script that starts a Flask web application
 """
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """displays Hello HBNB!"""
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """displays HBNB"""
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000
+    app.run(host='0.0.0.0', port=5000)
